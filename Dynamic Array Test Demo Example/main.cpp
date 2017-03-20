e654765bb606dd497a639f10c2309cbaa59b67f7
aberry5621 <aberry5621@gmail.com>
2017-03-19 17:26:36 -0700

    Initial Commit

---
+++ Dynamic Array Test Demo Example/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dynamic Array Test Demo Example
+//
+//  Created by ax on 3/19/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
