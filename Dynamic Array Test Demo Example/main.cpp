d618b07a272554d1b132bf9cdf22f1049a4297d1
aberry5621 <aberry5621@gmail.com>
2017-05-23 10:37:30 -0700

    For the repo

--- Dynamic Array Test Demo Example/main.cpp
+++ Dynamic Array Test Demo Example/main.cpp
@@ -8,8 +8,38 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main() {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Dynamic Array Test Demo Example!\n";
+    
+    // an array container
+    int * myDynamicArray = new int[10];
+    
+    // Populating array of 10, use array subscript
+    for (int i = 0; i < 10; i++) {
+        myDynamicArray[i] = i + 1;
+    }
+    
+    std::cout << "Array Contents: \n";
+    for (int i = 0; i < 10; i++) {
+        std::cout << myDynamicArray[i] << " ";
+    }
+    std::cout << std::endl;
+    
+    
+    // set up moving pointer
+    int * movingPtr = myDynamicArray;
+    
+    for (int i = 0; i < 20; i++) {
+        *movingPtr = i + 1;
+        movingPtr++;
+    }
+    
+    std::cout << "Array Contents: \n";
+    for (int i = 0; i < 20; i++) {
+        std::cout << myDynamicArray[i] << " ";
+    }
+    std::cout << std::endl;
+    
     return 0;
 }
